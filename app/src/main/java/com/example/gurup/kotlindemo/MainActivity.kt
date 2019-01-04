98bd3ee626937417bfc72bd2c8ad1c90d135cd7e
Even <evengrant1245@gmail.com>
2019-01-04 14:38:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/gurup/kotlindemo/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.gurup.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.gurup.kotlindemo.demofolder.Animal
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val TAG="DEMO";
+    var a=0;
+
+    var dummy="sa"
+    var ddd: String="asdsa";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        hello.setText("Hi")
+
+        dummy="assadasd"
+        ddd="sndnasd"
+
+
+        Log.d(TAG,"a : "+a);
+        display(a)
+
+        var animalObj=Animal("Zebra");
+        animalObj.animalDisplay("Animals")
+    }
+
+    fun display(name : Int){
+        var no=name+1;
+        Log.d(TAG,"display  : "+no);
+
+
+        var personObj =Person()
+        personObj.personDisplay(no);
+        personObj.names="Added"
+        personObj.personDisplayInstance(personObj.names)
+    }
+
+    class Person {
+        var names:String="";
+        fun personDisplay(name : Int){
+            Log.d("Inside public person" ,""+name);
+            names= name.toString();
+
+            Log.d("Inside public names" ,""+names);
+
+        }
+
+        fun personDisplayInstance(name : String){
+            Log.d("Inside person instance" ,""+name);
+
+
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/gurup/kotlindemo/demofolder/Animal.kt
@@ -0,0 +1,13 @@
+package com.example.gurup.kotlindemo.demofolder
+
+import android.util.Log
+
+class Animal(var names: String){
+
+
+     fun animalDisplay(x: String){
+
+         Log.d("Animal"," X;"+x);
+         Log.d("Animal "," Default animal"+names)
+     }
+ }
